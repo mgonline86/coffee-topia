62fcb14c42732fb7a631edbcd5932215a019b2fb
mgonline86 <mgonline86@yahoo.com>
2024-10-04 14:47:47 +0300

    UPDATE :Hero Section Styles

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,32 +1,23 @@
+import { Image } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
 import { Link } from "react-router-dom";
+import styles from "./HeroSection.module.css";
 
 const imagesPath = "img/carousel/";
 const imgExt = ".webp";
 
 const images = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-const directionButtons = (direction) => {
-  return (
-    <div className="p-2 bg-dark rounded-circle" style={{ width: "3rem", height: "3rem" }}>
-        <span
-          aria-hidden="true"
-          className={`carousel-control-${direction}-icon`}
-        ></span>
-    </div>
-  );
-};
-
 export default function HeroSection() {
   return (
-    <Carousel
-      indicators
-      nextIcon={directionButtons("next")}
-      prevIcon={directionButtons("prev")}
-    >
+    <Carousel indicators className={styles.heroCarousel}>
       {images.map((image) => (
         <Carousel.Item key={image} as={Link} to={"/products"}>
-          <img src={imagesPath + image + imgExt} alt={`Slide ${image}`} className="d-block w-100" />
+          <Image
+            src={imagesPath + image + imgExt}
+            alt={`Slide ${image}`}
+            fluid
+          />
         </Carousel.Item>
       ))}
     </Carousel>
