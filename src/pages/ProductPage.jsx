166332f8ed0a618c493a8a8c368931041340d9de
meltorky333 <meltorky333@gmail.com>
2024-10-08 22:59:43 +0300

    product page after update

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,7 +1,10 @@
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import ProductContext from "../contexts/productContext";
-import { Container } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 export default function ProductPage() {
   const { slug } = useParams();
@@ -12,10 +15,37 @@ export default function ProductPage() {
     return <div>Product not found</div>;
   }
   return (
-    <Container>
-      <div>{product.title}</div>
-      <div>EGP {product.price}</div>
-      <div dangerouslySetInnerHTML={{ __html: product.description }} />
+    <Container className="my-5">
+      <Row>
+        <Col xs={12} md={6}>
+          <Image src={product.image} width={400} height={400} fluid />
+        </Col>
+        <Col xs={12} md={6} className="d-flex flex-column gap-4">
+          <h2>{product.title} </h2>
+          <h3>LE {product.price} </h3>
+          <Row>
+            <Col xs={12} md={4} className="mb-3 mb-md-0">
+              <InputGroup className="h-100">
+                <Button variant="outline-secondary" id="button-addon1">
+                  -
+                </Button>
+                <Form.Control type="number" className="fs-5 text-center" />
+                <Button variant="outline-secondary" id="button-addon1">
+                  +
+                </Button>
+              </InputGroup>
+            </Col>
+
+            <Col xs={12} md={8}>
+              <Button variant="primary" size="lg" className="w-100">
+                Add to cart
+              </Button>
+            </Col>
+          </Row>
+
+          <p dangerouslySetInnerHTML={{ __html: product.description }} />
+        </Col>
+      </Row>
     </Container>
   );
 }
