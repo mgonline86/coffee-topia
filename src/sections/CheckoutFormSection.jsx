a9d4ae8a17ed9f219ec1340d05236c81cd76b602
mgonline86 <mgonline86@yahoo.com>
2024-10-12 15:51:49 +0300

    UPDATE: finish checkout process and Thank you

--- src/sections/CheckoutFormSection.jsx
+++ src/sections/CheckoutFormSection.jsx
@@ -1,149 +1,286 @@
+import { BanknoteIcon, CreditCardIcon } from "lucide-react";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import ThankYouSection from "./ThankYouSection";
 
 export default function CheckoutFormSection() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [phone, setPhone] = useState("");
-  const [paymentMethod, setPaymentMethod] = useState("cashOnDelivery");
-  const [cardNumber, setCardNumber] = useState("");
-  const [errors, setErrors] = useState({});
+  // Define states
+  const [showThankYou, setShowThankYou] = useState(false);
+  const [name, setName] = useState({
+    value: "",
+    isValid: false,
+    isTouched: false,
+    isDirty: false,
+  });
+  const [email, setEmail] = useState({
+    value: "",
+    isValid: false,
+    isTouched: false,
+    isDirty: false,
+  });
+  const [address, setAddress] = useState({
+    value: "",
+    isValid: false,
+    isTouched: false,
+    isDirty: false,
+  });
+  const [phone, setPhone] = useState({
+    value: "",
+    isValid: false,
+    isTouched: false,
+    isDirty: false,
+  });
+  const [paymentMethod, setPaymentMethod] = useState("Cash On Delivery");
+  const [cardNumber, setCardNumber] = useState({
+    value: "",
+    isValid: false,
+    isTouched: false,
+    isDirty: false,
+  });
 
-  const validateForm = () => {
-    const errors = {};
-    if (!name) {
-      errors.name = "Name is required";
-    }
-    if (!email) {
-      errors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      errors.email = "Email is invalid";
-    }
-    if (!address) {
-      errors.address = "Address is required";
-    }
-    if (!phone) {
-      errors.phone = "Phone is required";
-    } else if (!/^\d+$/.test(phone)) {
-      errors.phone = "Phone is invalid";
-    }
-    if (paymentMethod === "creditCard") {
-      if (!cardNumber) {
-        errors.cardNumber = "Card number is required";
-      } else if (!/^\d+$/.test(cardNumber)) {
-        errors.cardNumber = "Card number is invalid";
+  // Define state setters
+  const setters = {
+    name: setName,
+    email: setEmail,
+    address: setAddress,
+    phone: setPhone,
+    paymentMethod: setPaymentMethod,
+    cardNumber: (newState) => {
+      const trimmedValue = newState.value;
+      if (trimmedValue.length > 19) {
+        const alteredState = {
+          ...newState,
+          value: trimmedValue.slice(0, 19),
+        };
+        return setCardNumber((prev) => ({ ...prev, ...alteredState }));
       }
-    }
-
-    setErrors(errors);
-    return Object.keys(errors).length === 0;
+      return setCardNumber((prev) => ({ ...prev, ...newState }));
+    },
   };
 
+  // Define validations
+  const validations = {
+    name: (value) => value.length > 2, // Minimum 3 characters
+    email: (value) =>
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value), // Valid email format
+    address: (value) => value.length > 20, // Minimum 20 characters
+    phone: (value) => /^(\+201\d{9}|01\d{9})$/.test(value), // Valid Egyptian phone number format
+    cardNumber: (value) => {
+      const processedValue = value.slice(0, 19).replaceAll(" ", ""); // Remove whitespace and limit to 16 digits
+      return /^\d{16}$/.test(processedValue); // Valid card number format
+    },
+  };
+
+  // Validate form
+  const validateForm = (e) => {
+    const { id, value } = e.target;
+    const isValid = validations[id](value);
+    const isTouched = true;
+    const isDirty = value !== "";
+    setters[id]({
+      value: value.trim(),
+      isValid,
+      isTouched,
+      isDirty,
+    });
+  };
+
+  // Handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    if (!validateForm()) {
-      alert(JSON.stringify(errors));
+    let formValidations = [
+      name.isValid,
+      email.isValid,
+      address.isValid,
+      phone.isValid,
+    ];
+    if (paymentMethod === "Credit Card") {
+      formValidations.push(cardNumber.isValid);
+    }
+    const isFormValid = formValidations.every((valid) => valid);
+    if (!isFormValid) {
+      setName((prev) => ({ ...prev, isTouched: true }));
+      setEmail((prev) => ({ ...prev, isTouched: true }));
+      setAddress((prev) => ({ ...prev, isTouched: true }));
+      setPhone((prev) => ({ ...prev, isTouched: true }));
+      if (paymentMethod === "Credit Card") {
+        setCardNumber((prev) => ({
+          ...prev,
+          isTouched: true,
+        }));
+      }
       return;
     }
 
+    // Submit form
     const formData = {
-      name,
-      email,
-      address,
-      phone,
+      name: name.value,
+      email: email.value,
+      address: address.value,
+      phone: phone.value,
       paymentMethod,
-      cardNumber,
+      cardNumber: cardNumber.value.replaceAll(" ", ""),
     };
-
-    console.log(formData);
+    console.log("Form submitted successfully:\n", formData);
+    setShowThankYou(true);
   };
+
+  if (showThankYou) {
+    return (
+      <ThankYouSection
+        data={{
+          name: name.value,
+          email: email.value,
+          address: address.value,
+          phone: phone.value,
+          paymentMethod,
+          cardNumber: cardNumber.value,
+        }}
+      />
+    );
+  }
 
   return (
     <Form
-      className="bg-secondary text-primary rounded-4 p-5"
+      className="bg-secondary text-primary rounded-4 p-5 shadow"
       onSubmit={handleSubmit}
+      noValidate
     >
-      <Form.Group className="mb-3" controlId="formName">
+      <Form.Group className="mb-3" controlId="name">
         <Form.Label>Name</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter name"
+          placeholder="John Doe"
           required
-          onChange={(e) => setName(e.target.value)}
-          value={name}
+          onChange={validateForm}
+          value={name.value}
+          autoFocus
+          autoComplete="name"
+          isValid={name.isDirty && name.isValid}
+          isInvalid={name.isTouched && !name.isValid}
         />
+        {name.isTouched && !name.isValid && (
+          <Form.Control.Feedback type="invalid">
+            Name must be at least 3 characters
+          </Form.Control.Feedback>
+        )}
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formEmail">
+      <Form.Group className="mb-3" controlId="email">
         <Form.Label>Email address</Form.Label>
         <Form.Control
-          type="email"
-          placeholder="Enter email"
+          type="text"
+          placeholder="johndoe@example"
           required
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
+          onChange={validateForm}
+          value={email.value}
+          autoComplete="email"
+          isValid={email.isDirty && email.isValid}
+          isInvalid={email.isTouched && !email.isValid}
         />
+        {email.isTouched && !email.isValid && (
+          <Form.Control.Feedback type="invalid">
+            Invalid email
+          </Form.Control.Feedback>
+        )}
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formAddress">
+      <Form.Group className="mb-3" controlId="address">
         <Form.Label>Address</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter address"
+          placeholder="456 Elm St, Example City, USA, 90210"
           required
-          onChange={(e) => setAddress(e.target.value)}
-          value={address}
+          onChange={validateForm}
+          value={address.value}
+          autoComplete="address"
+          isValid={address.isDirty && address.isValid}
+          isInvalid={address.isTouched && !address.isValid}
         />
+        {address.isTouched && !address.isValid && (
+          <Form.Control.Feedback type="invalid">
+            Address must be at least 20 characters
+          </Form.Control.Feedback>
+        )}
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formPhone">
-        <Form.Label>Phone</Form.Label>
+      <Form.Group className="mb-3" controlId="phone">
+        <Form.Label>Mobile Number</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter phone"
+          placeholder="+20123456789"
           required
-          onChange={(e) => setPhone(e.target.value)}
-          value={phone}
+          onChange={validateForm}
+          value={phone.value}
+          autoComplete="phone"
+          isValid={phone.isDirty && phone.isValid}
+          isInvalid={phone.isTouched && !phone.isValid}
         />
+        {phone.isTouched && !phone.isValid && (
+          <Form.Control.Feedback type="invalid">
+            Invalid phone number
+          </Form.Control.Feedback>
+        )}
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formPaymentMethod">
-        <p className="mb-2">Payment Method</p>
+      <Form.Group className="mb-3">
+        <p className="mb-1">Payment Method</p>
         <Form.Check
           inline
-          label="Cash on Delivery"
+          className="d-inline-flex align-items-center gap-1"
+          label={
+            <>
+              <BanknoteIcon /> Cash on Delivery
+            </>
+          }
           name="paymentMethod"
           type="radio"
           id="cashOnDelivery"
-          onChange={() => setPaymentMethod("cashOnDelivery")}
+          onChange={() => setPaymentMethod("Cash On Delivery")}
           required
-          checked={paymentMethod === "cashOnDelivery"}
+          checked={paymentMethod === "Cash On Delivery"}
         />
         <Form.Check
           inline
-          label="Credit Card"
+          className="d-inline-flex align-items-center gap-1"
+          label={
+            <>
+              <CreditCardIcon /> Credit Card
+            </>
+          }
           name="paymentMethod"
           type="radio"
           id="creditCard"
-          onChange={() => setPaymentMethod("creditCard")}
+          onChange={() => setPaymentMethod("Credit Card")}
           required
-          checked={paymentMethod === "creditCard"}
+          checked={paymentMethod === "Credit Card"}
         />
       </Form.Group>
 
-      {paymentMethod === "creditCard" && (
-        <Form.Group className="mb-3" controlId="formCardNumber">
+      {paymentMethod === "Credit Card" && (
+        <Form.Group className="mb-3" controlId="cardNumber">
           <Form.Label>Card Number</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter card number"
+            placeholder="1234 5678 9012 3456"
             required
-            onChange={(e) => setCardNumber(e.target.value)}
-            value={cardNumber}
+            onChange={validateForm}
+            value={
+              cardNumber.value
+                ?.slice(0, 19)
+                ?.match(/\d{1,4}/g)
+                ?.join(" ") || ""
+            }
+            autoComplete="cc-number"
+            isValid={cardNumber.isDirty && cardNumber.isValid}
+            isInvalid={cardNumber.isTouched && !cardNumber.isValid}
           />
+          {cardNumber.isTouched && !cardNumber.isValid && (
+            <Form.Control.Feedback type="invalid">
+              Invalid card number
+            </Form.Control.Feedback>
+          )}
         </Form.Group>
       )}
 
