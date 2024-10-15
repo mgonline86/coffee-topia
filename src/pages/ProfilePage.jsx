2220d9ff2d82b230aeaac1a545635720fc4fb63f
mgonline86 <mgonline86@yahoo.com>
2024-10-15 18:10:40 +0300

    MERGE: m-gamal & m-ali Auth Updates

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,22 +3,27 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
+import useAuthContext from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function ProfilePage() {
-  const user = {
-    name: "Mohamed Ali",
-    email: "mohamed_ali@gmail.com",
-    image: "img/cartoon.png",
-    phone: "01013760783",
-    address: "125 Roshdy, abuquir st,alexandria",
-    orders: [],
-  };
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login", { replace: true });
+    }
+  });
+
   return (
     <Container>
       <Row>
-        <Col xs={12} md={6} className="mt-4 mb-3" >
+        <Col xs={12} md={6} className="mt-4 mb-3">
           {user?.image ? (
-            <Image className="rounded-5"
+            <Image
+              className="rounded-5"
               xs={12}
               md={6}
               width={500}
@@ -31,23 +36,30 @@ export default function ProfilePage() {
           )}
         </Col>
         <Col xs={12} md={6} className="my-5">
-          <p className="fs-4"> <IdCard className="me-2" />
+          <p className="fs-4">
+            {" "}
+            <IdCard className="me-2" />
             <span className="fw-bold me-2">Name:</span>
             {user.name}
           </p>
-          <p className="fs-4"> <Mail className="me-2"/>
+          <p className="fs-4">
+            {" "}
+            <Mail className="me-2" />
             <span className="fw-bold me-2">Email:</span>
             {user.email}
           </p>
-          <p className="fs-4"> <Phone className="me-2"/>
+          <p className="fs-4">
+            {" "}
+            <Phone className="me-2" />
             <span className="fw-bold me-2">Phone:</span>
             {user.phone}
           </p>
-          <p className="fs-4"> <MapPinHouseIcon className="me-2" />
+          <p className="fs-4">
+            {" "}
+            <MapPinHouseIcon className="me-2" />
             <span className="fw-bold me-2">Address:</span>
             {user.address}
           </p>
-          
         </Col>
       </Row>
     </Container>
