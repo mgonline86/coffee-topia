08f6e8484651120af5b712422b9b606409393e10
mgonline86 <mgonline86@yahoo.com>
2024-10-20 11:03:17 +0300

    UPDATE: Fancy Titles

--- src/sections/EmptyCartSection.jsx
+++ src/sections/EmptyCartSection.jsx
@@ -1,12 +1,13 @@
 import { ArrowLeftIcon } from "lucide-react";
 import { Button, Container, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import CoolTitle from "../components/CoolTitle";
 
 export default function EmptyCartSection({ closeFunction = null }) {
   return (
     <Container as="section" className="my-5 text-center">
       <div className="d-flex flex-column align-items-center gap-4">
-        <h1 className="text-primary fancyFont">Your cart is empty!</h1>
+        <CoolTitle title="Your cart is empty!" />
         <Image
           src="/img/coffee-cart.webp"
           style={{ filter: "drop-shadow(20px 0 10px #00000060)" }}
